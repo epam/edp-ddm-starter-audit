66e676c28ce5e9467bf348f555456af59a5b1062
Oleh_Soklakov <oleh_soklakov@epam.com>
2022-04-29 16:40:41 +0300

    [EDP-DDM-12547] extend model
    Change-Id: I2d52fd534f3ac54e726bf5dc0ec83f6b1363dca2

--- src/main/java/com/epam/digital/data/platform/starter/audit/model/EventType.java
+++ src/main/java/com/epam/digital/data/platform/starter/audit/model/EventType.java
@@ -17,5 +17,5 @@
 package com.epam.digital.data.platform.starter.audit.model;
 
 public enum EventType {
-    SECURITY_EVENT, USER_ACTION
+    SECURITY_EVENT, USER_ACTION, SYSTEM_EVENT
 }
---
+++ src/main/java/com/epam/digital/data/platform/starter/audit/model/Step.java
@@ -0,0 +1,5 @@
+package com.epam.digital.data.platform.starter.audit.model;
+
+public enum Step {
+  BEFORE, AFTER
+}
