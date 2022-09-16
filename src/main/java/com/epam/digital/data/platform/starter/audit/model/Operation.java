adc051694c12376db5b2bf9b0d3224bf667a0333
Vladyslav Chuhai <vladyslav_chuhai@epam.com>
2022-09-16 16:32:09 +0300

    [EDP-DDM-18810] added audit operations for notification channels
    Change-Id: I8a55e3c7ae4cb17f255eed756b5e9581c6a00fc9
    (cherry picked from commit c24cce7383d492c9724b8f40a2bb985973277821)

--- src/main/java/com/epam/digital/data/platform/starter/audit/model/Operation.java
+++ src/main/java/com/epam/digital/data/platform/starter/audit/model/Operation.java
@@ -1,5 +1,7 @@
 package com.epam.digital.data.platform.starter.audit.model;
 
 public enum Operation {
-  SEND_USER_NOTIFICATION
+  SEND_USER_NOTIFICATION,
+  USER_NOTIFICATION_CHANNEL_ACTIVATION,
+  USER_NOTIFICATION_CHANNEL_DEACTIVATION
 }
